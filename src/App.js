771b16a4c25bc43f8fb97d7f7cadfb59d01097ee
Cathrine <cathrinekamasho@gmail.com>
2025-02-03 13:17:57 +0200

    updated the files: App, weather.js and weather.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Weather from "./weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
+        <weather />
       <footer>
         This project was coded by Cathrine Kamasho and is {""}
         < a href='' target='_blank'>
---
+++ src/weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./weather.css";
+
+export default function weather (){
+    return (
+        <div className="weather">
+            <h1>Johannesburg</h1>
+            <ul>
+                <li>Sunday 13:15</li>
+                <li>Partly Cloudy</li>
+            </ul>
+        </div>
+    );
+}
