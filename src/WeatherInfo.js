a07e0eca0c47bd745b914d758914f99bb2a00cd2
Cathrine <cathrinekamasho@gmail.com>
2025-02-19 17:28:20 +0200

    added some bootstrap styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,7 +14,7 @@ export default function WeatherInfo(props){
                 <li className="text-capitalize">{props.data.description}</li>
             </ul>
             <div className="row">
-                <div className="col-6">
+                <div className="col-6 d-flex">
                     <WeatherIcon code={props.data.icon}/>
                     <WeatherTemperature celsius={props.data.temperature}/>
                     
