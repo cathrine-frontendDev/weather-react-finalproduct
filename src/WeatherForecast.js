a674edff7e0760ff3e227573508e1f7e78df4cfd
Cathrine <cathrinekamasho@gmail.com>
2025-02-22 19:29:20 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,8 +22,29 @@ export default function WeatherForecast (props){
                     <div className="col">
                    <WeatherForecastDay data={forecast[0]} />
                     </div>
-                </div>
-            </div>
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[1]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[2]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[3]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[4]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[5]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[6]} />
+                    </div> 
+                    <div className="col">
+                   <WeatherForecastDay data={forecast[7]} />
+                    </div> 
+              </div>
+          </div>
             );
    
     } else {
