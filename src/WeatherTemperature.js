2a5f479ac21d07d9e0084d4b4d8ef14db0e4034a
Cathrine <cathrinekamasho@gmail.com>
2025-03-06 10:33:32 +0200

    updated the weatherforecast and weathertemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,9 +4,7 @@ import React, {useState}from "react";
 
 export default function WeatherTemperature(props) {
     const [unit, setUnit]= useState("celsius");
-    function showFahrenheit(event) {
-        event.preventDefault();
-    }
+
 
     function showCelsius(event) {
         event.preventDefault();
